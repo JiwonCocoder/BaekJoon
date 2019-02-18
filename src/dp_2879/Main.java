60bd34d0eb7cb98c83092771e204ff999e02ceb3
iamad <iamad@110.15.203.73>
2019-02-18 23:41:50 +0900

    input ok

--- src/dp_2879/Main.java
+++ src/dp_2879/Main.java
@@ -1,21 +1,32 @@
 package dp_2879;
 import java.io.*;
 import java.util.*;
-
+import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		String line;
 		int count=0;
-		ArrayList<String> strList =new ArrayList<String>();
+		ArrayList<Integer> strList =new ArrayList<Integer>();
 		while((line=br.readLine())!=null){
-			line=line.trim();
-			String[] arr=line.split(" ");
-			//for(int i=;  )
-			strList.add(line);
+			System.out.println(line);
+			//if(line.isEmpty()) break;
+			StringTokenizer st = new StringTokenizer(line, " ");
+			while(st.hasMoreTokens()) {
+				strList.add(Integer.parseInt(st.nextToken()));
+			}
 		}
-		
-		//int[] startArray=new int[strList.get(0)];
+		System.out.println("here");
+
+		int arrayIndexAmount=strList.get(0);
+		int[] initArray= new int[arrayIndexAmount];
+		int[] destinationArray= new int[arrayIndexAmount];
+		for(int i=0; i<arrayIndexAmount;i++) {
+			int arrayListStartIndex=i+1;
+			initArray[i]=strList.get(arrayListStartIndex);
+			destinationArray[i]=strList.get(arrayListStartIndex+arrayIndexAmount);
+
+		}
 				
 
 	}
